6e83bc112d33558668848c8d5626cdc6f4feff23
Muhammad Ibaadi Ilmi <ibaadijaya2@gmail.com>
2026-03-06 19:38:45 +0700

    Implement basic home page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,46 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+
+function MarkTopUpSuccess() {
+    return (
+        <div>
+            <h1>Mark top-up request as successful</h1>
+            <form>
+                <label htmlFor="request-id">Top-up request id:</label>
+                <input type='number' id='request-id' required /><br />
+                <input type='submit' />
+            </form>
+        </div>
+    )
+}
+
+function MarkTopUpFailed() {
+    return (
+        <div>
+            <h1>Mark top-up request as failed</h1>
+            <form>
+                <label htmlFor="request-id">Top-up request id:</label>
+                <input type='number' id='request-id' required /><br />
+                <input type='submit' />
+            </form>
+        </div>
+    )
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+    return (
+        <BrowserRouter>
+            <nav>
+                <p><Link to="/mark-success">Mark top-up as successful</Link></p>
+                <p><Link to="/mark-failed">Mark top-up as failed</Link></p>
+            </nav>
 
-  return (
-    <h1>Halo dunia</h1>
-  )
+            <Routes>
+                <Route path="/mark-success" element={<MarkTopUpSuccess />} />
+                <Route path="/mark-failed" element={<MarkTopUpFailed />} />
+            </Routes>
+        </BrowserRouter>
+    )
 }
 
 export default App
